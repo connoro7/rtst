2b22a2b6bd5a5a9457f8b4c86eb96234f2dff0e0
Connor Dillon <connor@codewithconnor.com>
2021-05-04 16:54:19 -0700

    :sparkles: Added protobuf and buffer pkgs. Correctly decoding WS data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import { useEffect } from 'react'
+import protobuf from 'protobufjs'
+const { Buffer } = require('buffer/')
 
 function App() {
   useEffect(() => {
     const ws = new WebSocket('wss://streamer.finance.yahoo.com')
+    // const root = protobuf.loadSync('./YPricingData.proto')
+    protobuf.load('./YPricingData.proto', (error, root) => {
+      if (error) {
+        console.log(error)
+      }
+      const Yaticker = root.lookupType('yaticker')
 
-    ws.onopen = function open() {
-      console.log('connected')
+      ws.onopen = function open() {
+        console.log('connected')
 
-      ws.send(
-        JSON.stringify({
-          subscribe: ['MSFT'],
-        })
-      )
-    }
+        ws.send(
+          JSON.stringify({
+            subscribe: ['MSFT'],
+          })
+        )
+      }
 
-    ws.onclose = function close() {
-      console.log('disconnected')
-    }
+      ws.onclose = function close() {
+        console.log('disconnected')
+      }
 
-    ws.onmessage = function incoming(message) {
-      console.log('incoming data')
-      console.log(message.data)
-    }
+      ws.onmessage = function incoming(message) {
+        // console.log('incoming data')
+        // console.log(message.data)
+        // console.log(Yaticker.decode(new ArrayBuffer(message.data, 'base64')))
+        console.log(Yaticker.decode(new Buffer(message.data, 'base64')))
+      }
+    })
   }, [])
 
   return (
